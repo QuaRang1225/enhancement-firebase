eb11e4b87e0781956f4cd8c8eedc36284b636a13
QuaRang1225 <dbduddnd1225@gmail.com>
2023-06-12 14:26:35 +0900

    프로젝트 추가

---
+++ EX_Firebase/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EX_Firebase
+//
+//  Created by 유영웅 on 2023/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EX_Firebase/EX_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EX_FirebaseApp.swift
+//  EX_Firebase
+//
+//  Created by 유영웅 on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct EX_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
